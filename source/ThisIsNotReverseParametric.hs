00523d2ee600a9a45f8fc198c195568a4a47832f
Tony Morris <tmorris@tmorris.net>
2013-11-12 09:41:59 +1000

    slides for talk on parametricity

---
+++ source/ThisIsNotReverseParametric.hs
@@ -0,0 +1,15 @@
+module ThisIsNotReverse where
+
+-- | This function does not reverse.
+--
+-- >>> thisIsNotReverse []
+-- []
+--
+-- prop> (thisIsNotReverse . thisIsNotReverse) x == x
+--
+-- prop> thisIsNotReverse (x ++ y) == (thisIsNotReverse y ++ thisIsNotReverse x)
+thisIsNotReverse ::
+  [a]
+  -> [a]
+thisIsNotReverse =
+  error "todo"

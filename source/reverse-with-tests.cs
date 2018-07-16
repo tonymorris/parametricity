0592e6451a5c2e9f2c03f8ba351d01e59d52fd02
Tony Morris <tmorris@tmorris.net>
2018-07-16 11:00:04 +1000

    Rewrite

---
+++ source/reverse-with-tests.cs
@@ -0,0 +1,8 @@
+// csharp> function(List.empty) == List.empty
+//
+// csharp> x => function(function(x)) == x
+//
+// csharp> (x, y) => function(x.Append(y)) == function(y).Append(function(x))
+List<A> <A>function(List<A> x) {
+  $\ldots$
+}
